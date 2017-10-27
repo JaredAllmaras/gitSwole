94cb9af4f0debfcb6007cfd02c74bea523a38c3a
namaya <nkamaya7@gmail.com>
2017-10-26 19:28:39 -0500

    Setup Firebase and CocoaPods.

---
+++ gitSwole/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitSwole
+//
+//  Created by Nicholas Amaya on 10/26/17.
+//  Copyright © 2017 gitSwoleLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
