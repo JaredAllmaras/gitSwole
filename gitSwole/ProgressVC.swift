66e41313534719ec7775604f3c73bbaace9d62e5
namaya <nkamaya7@gmail.com>
2017-10-31 23:43:06 -0500

    mes

--- gitSwole/ProgressVC.swift
+++ gitSwole/ProgressVC.swift
@@ -21,16 +21,24 @@ class ProgressVC: UIViewController {
     @IBOutlet weak var lastUpdated: UILabel!
     
     @IBAction func saveGoal(_ sender: Any) {
-        if currentWeight.text != "" {
-            currentWeightLabel.text = "\(currentWeight.text!) lbs"
-        }
-        if goalWeight.text != "" {
-            goalWeightLabel.text = "\(goalWeight.text!) lbs"
-        }
-        let intGoal:Int = Int(goalWeightLabel.text!)!
-        let intCurrent:Int = Int(currentWeightLabel.text!)!
         
-        difference.text = "\(String(describing: (intGoal - intCurrent))) lbs"
+        let intGoal = Int(goalWeight.text!)
+        if intGoal == nil {
+            print("Invalid goal weight")
+            return
+        } else {
+            goalWeightLabel.text = "\(goalWeight.text!)"
+        }
+        
+        let intCurrent = Int(currentWeight.text!)
+        if intCurrent == nil {
+            print("Invalid weight")
+            return
+        } else {
+            currentWeightLabel.text = "\(currentWeight.text!)"
+        }
+        
+        difference.text = "\(String(describing: (intGoal! - intCurrent!)))"
 
         let date = Date()
         let formatter = DateFormatter()
@@ -53,6 +61,27 @@ class ProgressVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // This method is called when the user touches the Return key on the
+    // keyboard. The 'textField' passed in is a pointer to the textField
+    // the cursor was in at the time they touched the Return key on the
+    // keyboard.
+    //
+    // From the Apple documentation: Asks the delegate if the text field
+    // should process the pressing of the return button.
+    //
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 'First Responder' is the same as 'input focus'.
+        // We are removing input focus from the text field.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Called when the user touches on the main view (outside the UITextField).
+    //
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 
